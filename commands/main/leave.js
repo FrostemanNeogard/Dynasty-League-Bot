890d2e09689d519b45a15a74b95ad7933d48a016
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-02-14 18:00:02 +0100

    Create leave command

---
+++ commands/main/leave.js
@@ -0,0 +1,48 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  name: "leave",
+  category: "main",
+  permissions: [],
+  devCommand: false,
+  data: new SlashCommandBuilder()
+    .setName("leave")
+    .setDescription("Leave your assigned groupchat."),
+
+  async execute(interaction, client) {
+    const guild = interaction.guild;
+    const member = interaction.member;
+
+    if (!member) {
+      return await interaction.reply({
+        content: `Invalid user.`,
+        ephemeral: true,
+      });
+    }
+
+    const groupchatChannelRegex = /^groupchat-\d+$/;
+    const groupchatRoles = member.roles.cache.find((role) =>
+      groupchatChannelRegex.test(role.name)
+    );
+
+    if (groupchatRoles.length < 1) {
+      return await interaction.reply({
+        content: `You are not in any groupchats.`,
+        ephemeral: true,
+      });
+    }
+
+    const groupchatName = [...groupchatRoles][0];
+
+    const channelRole = guild.roles.cache.find(
+      (role) => role.name == groupchatName
+    );
+
+    await guild.members.cache.get(member.id).roles.remove(channelRole);
+
+    return await interaction.reply({
+      content: `You have been successfully removed from the following groupchat: ${groupchatName}.`,
+      ephemeral: true,
+    });
+  },
+};
