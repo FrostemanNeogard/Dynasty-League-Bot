773b485bffbfbf7a4fb673384d6b29ff88745b60
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-02-21 02:15:17 +0100

    Change logic to allow users to leave a specified groupchat

--- commands/main/leave.js
+++ commands/main/leave.js
@@ -7,16 +7,29 @@ module.exports = {
   devCommand: false,
   data: new SlashCommandBuilder()
     .setName("leave")
-    .setDescription("Leave your assigned groupchat."),
+    .setDescription("Leave a specified groupchat.")
+    .addChannelOption((option) =>
+      option
+        .setName("groupchat")
+        .setDescription("The groupchat to leave.")
+        .setRequired(true)
+    ),
 
   async execute(interaction, client) {
     const guild = interaction.guild;
     const member = interaction.member;
+    const groupchat = interaction.options.getChannel("groupchat");
 
     if (!member) {
       return await interaction.reply({
         content: `Invalid user.`,
-        ephemeral: true,
+        ephemeral: false,
+      });
+    }
+    if (!groupchat) {
+      return await interaction.reply({
+        content: `No groupchat was found.`,
+        ephemeral: false,
       });
     }
 
@@ -28,21 +41,19 @@ module.exports = {
     if (!groupchatRoles) {
       return await interaction.reply({
         content: `You are not in any groupchats.`,
-        ephemeral: true,
+        ephemeral: false,
       });
     }
 
-    const groupchatName = groupchatRoles.name;
-
     const channelRole = guild.roles.cache.find(
-      (role) => role.name == groupchatName
+      (role) => role.name == groupchat.name
     );
 
     await guild.members.cache.get(member.id).roles.remove(channelRole);
 
     return await interaction.reply({
-      content: `You have been successfully removed from the following groupchat: ${groupchatName}.`,
-      ephemeral: true,
+      content: `You have been successfully removed from the following groupchat: ${groupchat}.`,
+      ephemeral: false,
     });
   },
 };
