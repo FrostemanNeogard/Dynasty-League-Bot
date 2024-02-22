19d5092434943fc0d3bdcb555df850c9421b828b
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-02-22 16:42:00 +0100

    Add capitalizeFirstLetters function

--- util/functions.js
+++ util/functions.js
@@ -3,6 +3,18 @@ const getFiles = (path, ending) => {
   return fs.readdirSync(path).filter((f) => f.endsWith(ending));
 };
 
+const capitalizeFirstLetters = (string) => {
+  const words = string.split(" ");
+  const output = [];
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    const capitalizedWord = word[0].toUpperCase() + word.substring(1);
+    output.push(capitalizedWord);
+  }
+  return output.join(" ");
+};
+
 module.exports = {
   getFiles,
+  capitalizeFirstLetters,
 };
